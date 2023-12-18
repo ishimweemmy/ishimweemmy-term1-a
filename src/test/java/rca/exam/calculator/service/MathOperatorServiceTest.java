0688f1446b69c40f5caa3381270c33dc8efd7182
ishimweemmy <ishimweemmy24@gmail.com>
2023-12-18 12:07:32 +0200

    [feat]: refactored unit test

--- src/test/java/rca/exam/calculator/service/MathOperatorServiceTest.java
+++ src/test/java/rca/exam/calculator/service/MathOperatorServiceTest.java
@@ -23,9 +23,48 @@ public class MathOperatorServiceTest {
     @Test
     public void OnAdditionOperation_Should_AddProperly() throws InvalidOperationException {
         double operator1 = 30;
-        double operator2 = 9;
+        double operator2 = 10;
         String operation = "+";
-        double expectedResult = 39.0;
+        double expectedResult = 40.0;
+
+        when(mathOperatorService.doMath(operator1, operator2, operation)).thenReturn(expectedResult);
+        double actualMathResult = mathOperatorService.doMath(operator1, operator2, operation);
+        assertThat(actualMathResult).isEqualTo(expectedResult);
+        verify(mathOperatorService).doMath(operator1, operator2, operation);
+    }
+
+        @Test
+    public void OnSubtractionOperation_Should_SubtractProperly() throws InvalidOperationException {
+        double operator1 = 30;
+        double operator2 = 10;
+        String operation = "-";
+        double expectedResult = 20.0;
+
+        when(mathOperatorService.doMath(operator1, operator2, operation)).thenReturn(expectedResult);
+        double actualMathResult = mathOperatorService.doMath(operator1, operator2, operation);
+        assertThat(actualMathResult).isEqualTo(expectedResult);
+        verify(mathOperatorService).doMath(operator1, operator2, operation);
+    }
+
+        @Test
+    public void OnMultiplicationOperation_Should_MultiplyProperly() throws InvalidOperationException {
+        double operator1 = 30;
+        double operator2 = 10;
+        String operation = "*";
+        double expectedResult = 300.0;
+
+        when(mathOperatorService.doMath(operator1, operator2, operation)).thenReturn(expectedResult);
+        double actualMathResult = mathOperatorService.doMath(operator1, operator2, operation);
+        assertThat(actualMathResult).isEqualTo(expectedResult);
+        verify(mathOperatorService).doMath(operator1, operator2, operation);
+    }
+
+        @Test
+    public void OnDivisionOperation_Should_DivideProperly() throws InvalidOperationException {
+        double operator1 = 30;
+        double operator2 = 10;
+        String operation = "/";
+        double expectedResult = 3.0;
 
         when(mathOperatorService.doMath(operator1, operator2, operation)).thenReturn(expectedResult);
         double actualMathResult = mathOperatorService.doMath(operator1, operator2, operation);
